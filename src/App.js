4995f6852c2875ee72e5a66944fb7dc9629b6518
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2022-01-16 12:04:31 -0500

    working on the stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {Typography} from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <Typography style={{color:'pink'}} variant='h2' color='pink' noWrap >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores inventore debitis quis doloremque nemo nihil provident nesciunt quae animi. Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta adipisci autem velit necessitatibus nisi recusandae dolorem repellat accusantium ipsam quidem?
+      </Typography>
     </div>
   );
 }
